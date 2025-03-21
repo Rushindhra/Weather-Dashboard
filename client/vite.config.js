dc3c7eb72ba1776f42cce098d83dcff6fbd45790
Rushindhra <138361366+Rushindhra@users.noreply.github.com>
2025-03-22 01:18:05 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default defineConfig({
+  optimizeDeps: {
+    include: ["country-state-city"],
+  },
+});
